69c087e4c72d1fc96fccceea51e0c22b6d90cb41
kyunghwanma <makh1213@naver.com>
2022-05-08 21:51:58 +0900

    랜딩 페이지에 비디오들 나타나게 하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,64 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import { Avatar, Col, Row, Typography, Card } from "antd";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import { duration } from "moment";
+
+const { Title } = Typography;
+const { Meta } = Card;
 
 function LandingPage() {
+  const [Video, setVideo] = useState([]);
+  useEffect(() => {
+    Axios.get("/api/video/getVideos").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setVideo(response.data.videos);
+      } else {
+        alert("비디오 가져오기를 실패했습니다.");
+      }
+    });
+  }, []);
+
+  const renderCards = Video.map((video, index) => {
+    let minutes = Math.floor(video.duration / 60);
+    let seconds = Math.floor(video.duration - minutes * 60);
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+      <Col lg={6} md={8} xs={24} key={index}>
+        <a href={`/video/post/${video._id}`}>
+          <div style={{ position: "relative" }}>
+            <img
+              style={{ width: "100%" }}
+              src={`http://localhost:5000/${video.thumbnail}`}
+            />
+            <div className="duration">
+              <span>
+                {minutes} : {seconds}
+              </span>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+          </div>
+        </a>
+        <br />
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+          description=""
+        />
+        <span>{video.title}</span>
+        <span style={{ marginLeft: "3rem" }}>{video.views} views</span> ={" "}
+        <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <Title level={2}> Recommened</Title>
+      <hr />
+      <Row gutter={[32, 16]}>{renderCards}</Row>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
