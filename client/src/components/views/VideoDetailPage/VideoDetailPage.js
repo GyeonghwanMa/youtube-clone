e31567dd4b6e0d73502db6b60da1d17e3deb6c55
kyunghwanma <makh1213@naver.com>
2022-05-16 21:22:02 +0900

    댓글 기능 생성(1)

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,6 +4,7 @@ import Axios from "axios";
 
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
+import Comment from "./Sections/Comment";
 
 function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -24,6 +25,11 @@ function VideoDetailPage(props) {
     });
   }, []);
 
+  const subscribeButton = VideoDetail.writer?._id !==
+    localStorage.getItem("userId") && (
+    <Subscribe userTo={VideoDetail.writer?._id} />
+  );
+
   return (
     <Row gutter={[16, 16]}>
       <Col lg={18} xs={24}>
@@ -34,7 +40,7 @@ function VideoDetailPage(props) {
             controls
           />
 
-          <List.Item actions={[<Subscribe userTo={VideoDetail.writer?._id} />]}>
+          <List.Item actions={[subscribeButton]}>
             <List.Item.Meta
               avatar={<Avatar src={VideoDetail.writer?.image} />}
               title={VideoDetail.writer?.name}
@@ -43,6 +49,7 @@ function VideoDetailPage(props) {
           </List.Item>
 
           {/* Comments */}
+          <Comment />
         </div>
       </Col>
       <Col lg={6} xs={24}>
