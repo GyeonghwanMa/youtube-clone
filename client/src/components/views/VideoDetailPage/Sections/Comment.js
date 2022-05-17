a97676b1f21172abf5e0cfe574838b2eb9855616
kyunghwanma <makh1213@naver.com>
2022-05-17 19:38:46 +0900

    댓글 기능 생성(2)

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useSelector } from "react-redux";
 
-function Comment() {
-  return <div>Comment</div>;
+function Comment(props) {
+  const user = useSelector((state) => state.user); // redux에서 가져오기
+  const videoId = props.postId;
+  const [CommentValue, setCommentValue] = useState("");
+  const handleClick = (e) => {
+    setCommentValue(e.currentTarget.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variables = {
+      content: CommentValue,
+      writer: user.userData._id,
+      postId: videoId,
+    };
+    Axios.post("/api/comment/saveComment", variables).then((response) => {
+      if (response.data.success) {
+        console.log(response.data.result);
+      } else {
+        alert("코멘트를 저장하지 못했습니다.");
+      }
+    });
+  };
+  return (
+    <div>
+      <br />
+      <p>Replies</p>
+      <hr />
+
+      {/* Comment Lists */}
+
+      {/* Root Comment Form */}
+
+      <form style={{ display: "flex" }} onSubmit={onSubmit}>
+        <textarea
+          style={{ width: "100%", borderRadius: "5px" }}
+          onChange={handleClick}
+          value={CommentValue}
+          placeholder="코멘트를 작성해주세요"
+        />
+        <br />
+        <button style={{ width: "20%", height: "52px" }} onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Comment;
